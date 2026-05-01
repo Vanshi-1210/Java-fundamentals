45902d874da562fa075b59e2f917b8bab218f4c3
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-05-01 15:45:07 +0530

    Add Java program for practicing System.out.println and star patterns

--- OutputPrintingPractice.java
+++ OutputPrintingPractice.java
@@ -1,3 +1,5 @@
+// This program is for practicing basic output statements in Java
+// It prints simple star patterns using System.out.println()
 public class OutputPrintingPractice {
     public static void main(String[] args) {
         System.out.println("*");
