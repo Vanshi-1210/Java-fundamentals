dfca20f6d6fae5cfdf056ae5e1a4b22dbb56ccd6
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-05-02 14:54:45 +0530

    Added inverted half pyramid pattern program

---
+++ InvertedHalfPyramidPattern.java
@@ -0,0 +1,12 @@
+// Program to print an inverted half pyramid pattern
+public class InvertedHalfPyramidPattern {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
