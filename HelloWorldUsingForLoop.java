d8a73845cbbf92d616d1d2e63a55cfe3b2353c5a
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-05-02 08:19:26 +0530

    Added Hello World program using for loop

---
+++ HelloWorldUsingForLoop.java
@@ -0,0 +1,9 @@
+// Program to print Hello World 100 times
+// using a for loop
+public class HelloWorldUsingForLoop {
+    public static void main(String[] args) {
+        for(int i=0;i<=100;i++){
+            System.out.println("Hello World");
+        }
+    }
+}
