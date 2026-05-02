248764a4902a5107d81a3895fed01f06c6a0918a
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-05-02 09:06:04 +0530

    Added program to print numbers from 1 to 10 using do-while loop

---
+++ PrintNumbersUsingDoWhileLoop.java
@@ -0,0 +1,11 @@
+// Program to print numbers from 1 to 10
+// using a do-while loop
+public class PrintNumbersUsingDoWhileLoop {
+   public static void main(String[] args) {
+    int i=1;
+    do{
+        System.out.println(i);
+        i++;
+    }while(i<=10);
+   } 
+}
