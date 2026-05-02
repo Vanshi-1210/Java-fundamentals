6e96dc1b3f6062800cdc8ccfab3a7dcb8520bc33
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-05-02 18:20:15 +0530

    Added solid rhombus pattern program

---
+++ SolidRhombusPattern.java
@@ -0,0 +1,15 @@
+// Program to print a solid rhombus pattern
+public class SolidRhombusPattern {
+  public static void main(String[] args) {
+   int n=5;
+   for(int i=1;i<=n;i++){
+    for(int j=1;j<=n-i;j++){
+     System.out.print(" ");
+    }
+    for(int j=1;j<=n;j++){
+        System.out.print("*");
+   }
+System.out.println();
+}
+  }
+}
